4f58abe60778211842ae8b651afa9c31d501a695
정원종 <jwj970802@gmail.com>
2025-07-19 02:05:12 +0900

    fix: test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,6 @@ export default function Home() {
       <ProjectsSection/>
       <LinksSection/>
       <ContactSection/>
-      <div className="prose prose-invert p-8">
-      <h1>Markdown 제목</h1>
-      <p>문단입니다. <strong>강조</strong>도 있습니다.</p>
-      <ul>
-        <li>리스트 1</li>
-        <li>리스트 2</li>
-      </ul>
-    </div>
     </main>
     </>
   );
